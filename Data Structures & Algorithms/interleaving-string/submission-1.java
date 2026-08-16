1461250804d37e7cc90d8cf6c51b60e5c7bde094
Vikas-o-Pawar <133585029+Vikas-o-Pawar@users.noreply.github.com>
2026-08-17 00:01:05 +0530

    Add: interleaving-string - submission-1

---
+++ Data Structures & Algorithms/interleaving-string/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if(s1.length() + s2.length()  != s3.length()) return false;
+        return dfs(0, 0, s1, s2, s3, new HashMap<String, Boolean>());
+    }
+
+    public boolean dfs(int i, int j, String s1, String s2, String s3, Map<String, Boolean> map) {
+        if (i == s1.length() && j == s2.length()) return true;
+        String key = i + ", " + j;
+
+        if(map.containsKey(key)) {
+            return map.get(key);
+        }
+
+        if(i < s1.length() && s1.charAt(i) == s3.charAt(i + j) && dfs(i + 1, j, s1, s2, s3, map)) {
+            return true;
+        }
+
+        if(j < s2.length() && s2.charAt(j) == s3.charAt(i + j) && dfs(i, j + 1, s1, s2, s3, map)) {
+            return true;
+        }
+
+        map.put(key, false);
+        return false;
+    }
+}
